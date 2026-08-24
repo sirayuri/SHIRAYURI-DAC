27eda69b8b2c68a6ec7b0de6346add344103a447
sirayuri <118947615+sirayuri@users.noreply.github.com>
2026-08-25 01:02:37 +0900

    Correct the UAC1 configuration descriptor size

--- Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Inc/usbd_audio.h
+++ Middlewares/ST/STM32_USB_Device_Library/Class/AUDIO/Inc/usbd_audio.h
@@ -64,7 +64,7 @@ extern "C" {
 #define AUDIO_IN_EP                                   0x82U//FEEDBACK END POINT
 
 //#define USB_AUDIO_CONFIG_DESC_SIZ                     0x6DU
-#define USB_AUDIO_CONFIG_DESC_SIZ                     0x76U//★add 9 bytes
+#define USB_AUDIO_CONFIG_DESC_SIZ                     0x77U /* UAC1 stereo + feedback */
 #define AUDIO_INTERFACE_DESC_SIZE                     0x09U
 #define USB_AUDIO_DESC_SIZ                            0x09U
 #define AUDIO_STANDARD_ENDPOINT_DESC_SIZE             0x09U
